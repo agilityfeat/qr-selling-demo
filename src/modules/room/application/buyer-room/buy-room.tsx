c9b8e6248e3808a40620b405425c57b9d2f52a04
Hector Zelaya <inge.zelaya@gmail.com>
2023-06-26 17:01:32 -0600

    added player

--- src/modules/room/application/buyer-room/buy-room.tsx
+++ src/modules/room/application/buyer-room/buy-room.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Script from 'next/script'
+import { ivsConfig } from '@/config/ivs'
 import BuyRoomView from './buy-room.view'
 
 const BuyRoom = function BuyRoom() {
-	return <BuyRoomView />
+	const player = useRef<any>()
+	const videoRef = useRef<HTMLVideoElement>(null)
+
+	return (
+		<>
+			<Script
+				src="https://player.live-video.net/1.19.0/amazon-ivs-player.min.js"
+				strategy="afterInteractive"
+				onLoad={() => {
+					// @ts-ignore
+					const IVSPackage = window.IVSPlayer
+					player.current = IVSPackage.create()
+
+					player.current.attachHTMLVideoElement(
+						videoRef.current as HTMLVideoElement
+					)
+					player.current.load(ivsConfig.playbackUrl)
+					player.current.play()
+				}}
+			/>
+			<BuyRoomView videoRef={videoRef} />
+		</>
+	)
 }
 
 export default BuyRoom
